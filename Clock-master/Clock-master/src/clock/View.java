c59185de66d31dd6bed56c77f1fdf381c8e139c6
FireHunter3958 <47977209+FireHunter3958@users.noreply.github.com>
2019-04-22 13:46:16 +0100

    Add files via upload
    Master file, with some basic work (skeleton for menu system and priority queue already implemented, requires further work).

---
+++ Clock-master/Clock-master/src/clock/View.java
@@ -0,0 +1,156 @@
+package clock;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.Observer;
+import java.util.Observable;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
+import javax.swing.JMenuBar;
+import javax.swing.KeyStroke;
+import javax.swing.ImageIcon;
+
+public class View implements Observer {
+    
+    ClockPanel panel;
+    ClockPanel pane2;
+    
+    public View(Model model) {
+        JFrame frame = new JFrame();
+        panel = new ClockPanel(model);
+        pane2 = new ClockPanel(model);
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
+
+        //frame.setContentPane(panel);
+        frame.setTitle("Java Clock");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        //Create the menu bar.
+        menuBar = new JMenuBar();
+
+        //Build the first menu.
+        menu = new JMenu("A Menu");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+        //a group of JMenuItems
+        menuItem = new JMenuItem("A text-only menu item",
+                                 KeyEvent.VK_T);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem("Both text and icon",
+                                 new ImageIcon("images/middle.gif"));
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
+        menuItem.setMnemonic(KeyEvent.VK_D);
+        menu.add(menuItem);
+
+        //a group of radio button menu items
+        menu.addSeparator();
+        ButtonGroup group = new ButtonGroup();
+        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
+        rbMenuItem.setSelected(true);
+        rbMenuItem.setMnemonic(KeyEvent.VK_R);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        rbMenuItem = new JRadioButtonMenuItem("Another one");
+        rbMenuItem.setMnemonic(KeyEvent.VK_O);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        //a group of check box menu items
+        menu.addSeparator();
+        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
+        cbMenuItem.setMnemonic(KeyEvent.VK_C);
+        menu.add(cbMenuItem);
+
+        cbMenuItem = new JCheckBoxMenuItem("Another one");
+        cbMenuItem.setMnemonic(KeyEvent.VK_H);
+        menu.add(cbMenuItem);
+
+        //a submenu
+        menu.addSeparator();
+        submenu = new JMenu("A submenu");
+        submenu.setMnemonic(KeyEvent.VK_S);
+
+        menuItem = new JMenuItem("An item in the submenu");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Another item");
+        submenu.add(menuItem);
+        menu.add(submenu);
+
+        //Build second menu in the menu bar.
+        menu = new JMenu("Another Menu");
+        menu.setMnemonic(KeyEvent.VK_N);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "This menu does nothing");
+        menuBar.add(menu);
+
+        
+        frame.setJMenuBar(menuBar);
+        // Start of border layout code
+        
+        // I've just put a single button in each of the border positions:
+        // PAGE_START (i.e. top), PAGE_END (bottom), LINE_START (left) and
+        // LINE_END (right). You can omit any of these, or replace the button
+        // with something else like a label or a menu bar. Or maybe you can
+        // figure out how to pack more than one thing into one of those
+        // positions. This is the very simplest border layout possible, just
+        // to help you get started.
+        
+        Container pane = frame.getContentPane();
+        
+        JButton button = new JButton("Button 1 (PAGE_START)");
+        /*
+        pane.add(button, BorderLayout.PAGE_START);
+         */
+        panel.setPreferredSize(new Dimension(200, 200));
+        pane.add(panel, BorderLayout.CENTER);
+         
+        button = new JButton("Button 3 (LINE_START)");
+        pane.add(button, BorderLayout.LINE_START);
+         
+        button = new JButton("Long-Named Button 4 (PAGE_END)");
+        pane.add(button, BorderLayout.PAGE_END);
+         
+        button = new JButton("5 Add Alarm(LINE_END)");
+        pane.add(button, BorderLayout.LINE_END);
+        /*
+        button = new JButton("5 Add Alarm(LINE_END)");
+        pane.add(button, BorderLayout.BEFORE_FIRST_LINE);
+        */
+        pane.add(menuBar, BorderLayout.PAGE_START);
+        
+        // End of borderlayout code
+        
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public void update(Observable o, Object arg) {
+        panel.repaint();
+    }
+}
