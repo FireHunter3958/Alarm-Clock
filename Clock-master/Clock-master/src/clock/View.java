cb0a4944437c430ca101b47f98a2bc1d4a920e75
FireHunter3958 <47977209+FireHunter3958@users.noreply.github.com>
2019-04-26 14:37:00 +0100

    Add files via upload
    Menu system now provides feedback to the user (must be changed to perform actions when pressed).

--- Clock-master/Clock-master/src/clock/View.java
+++ Clock-master/Clock-master/src/clock/View.java
@@ -16,15 +16,18 @@ import javax.swing.JMenuBar;
 import javax.swing.KeyStroke;
 import javax.swing.ImageIcon;
 
-public class View implements Observer {
-    
+public class View implements Observer, ActionListener, ItemListener {
+    PriorityQueue<Person> q;
     ClockPanel panel;
     ClockPanel pane2;
+    JTextArea output;
+    JScrollPane scrollPane;
+    String newline = "\n";
     
     public View(Model model) {
         JFrame frame = new JFrame();
         panel = new ClockPanel(model);
-        pane2 = new ClockPanel(model);
+        //pane2 = new ClockPanel(model);
         JMenuBar menuBar;
         JMenu menu, submenu;
         JMenuItem menuItem;
@@ -47,68 +50,28 @@ public class View implements Observer {
         menuBar.add(menu);
 
         //a group of JMenuItems
+        JMenuItem menuItemV2;
+        menuItemV2 = new JMenuItem(new MyAction());
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuItemV2.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItemV2.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        q = new SortedPriorityQueue();
+        menuItemV2.addActionListener(this);
+        menu.add(menuItemV2);
+        
+        //a group of JMenuItems
         menuItem = new JMenuItem("A text-only menu item",
                                  KeyEvent.VK_T);
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
         menuItem.setAccelerator(KeyStroke.getKeyStroke(
                 KeyEvent.VK_1, ActionEvent.ALT_MASK));
         menuItem.getAccessibleContext().setAccessibleDescription(
                 "This doesn't really do anything");
+        menuItem.addActionListener(this);
         menu.add(menuItem);
-
-        menuItem = new JMenuItem("Both text and icon",
-                                 new ImageIcon("images/middle.gif"));
-        menuItem.setMnemonic(KeyEvent.VK_B);
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
-        menuItem.setMnemonic(KeyEvent.VK_D);
-        menu.add(menuItem);
-
-        //a group of radio button menu items
-        menu.addSeparator();
-        ButtonGroup group = new ButtonGroup();
-        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_R);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        rbMenuItem = new JRadioButtonMenuItem("Another one");
-        rbMenuItem.setMnemonic(KeyEvent.VK_O);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        //a group of check box menu items
-        menu.addSeparator();
-        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
-        cbMenuItem.setMnemonic(KeyEvent.VK_C);
-        menu.add(cbMenuItem);
-
-        cbMenuItem = new JCheckBoxMenuItem("Another one");
-        cbMenuItem.setMnemonic(KeyEvent.VK_H);
-        menu.add(cbMenuItem);
-
-        //a submenu
-        menu.addSeparator();
-        submenu = new JMenu("A submenu");
-        submenu.setMnemonic(KeyEvent.VK_S);
-
-        menuItem = new JMenuItem("An item in the submenu");
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_2, ActionEvent.ALT_MASK));
-        submenu.add(menuItem);
-
-        menuItem = new JMenuItem("Another item");
-        submenu.add(menuItem);
-        menu.add(submenu);
-
-        //Build second menu in the menu bar.
-        menu = new JMenu("Another Menu");
-        menu.setMnemonic(KeyEvent.VK_N);
-        menu.getAccessibleContext().setAccessibleDescription(
-                "This menu does nothing");
-        menuBar.add(menu);
-
+        
         
         frame.setJMenuBar(menuBar);
         // Start of border layout code
@@ -130,13 +93,21 @@ public class View implements Observer {
         panel.setPreferredSize(new Dimension(200, 200));
         pane.add(panel, BorderLayout.CENTER);
          
-        button = new JButton("Button 3 (LINE_START)");
+        button = new JButton("Button 3 (Add Alarm)");
         pane.add(button, BorderLayout.LINE_START);
-         
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
+        q = new SortedLinkedPriorityQueue<>();
+        
+        output = new JTextArea(5, 30);
+        output.setEditable(false);
+        JScrollPane paneScroll = new JScrollPane(output);
+
+        //Add the text area to the content pane.
+        pane.add(paneScroll, BorderLayout.PAGE_END);
+         /*
+        button = new JButton("End Program (End Program)");
         pane.add(button, BorderLayout.PAGE_END);
-         
-        button = new JButton("5 Add Alarm(LINE_END)");
+         */
+        button = new JButton("5 Add Alarm(Edit Alarm)");
         pane.add(button, BorderLayout.LINE_END);
         /*
         button = new JButton("5 Add Alarm(LINE_END)");
@@ -153,4 +124,165 @@ public class View implements Observer {
     public void update(Observable o, Object arg) {
         panel.repaint();
     }
+    
+    public Container createContentPane() {
+        //Create the content-pane-to-be.
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setOpaque(true);
+
+        //Create a scrolled text area.
+        output = new JTextArea(5, 30);
+        output.setEditable(false);
+        scrollPane = new JScrollPane(output);
+
+        //Add the text area to the content pane.
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+
+        return contentPane;
+    }
+    
+    protected class MyAction extends AbstractAction {
+        public MyAction() {
+        super ("My Menu Item");
+                }
+        public void actionPerformed(ActionEvent e) {
+            q = new SortedPriorityQueue();
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JMenuItem source = (JMenuItem)(e.getSource());
+        String s = "Action event detected."
+                   + newline
+                   + "    Event source: " + source.getText()
+                   + " (an instance of " + getClassName(source) + ")";
+        output.append(s + newline);
+        output.setCaretPosition(output.getDocument().getLength());
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        JMenuItem source = (JMenuItem)(e.getSource());
+        String s = "Item event detected."
+                   + newline
+                   + "    Event source: " + source.getText()
+                   + " (an instance of " + getClassName(source) + ")"
+                   + newline
+                   + "    New state: "
+                   + ((e.getStateChange() == ItemEvent.SELECTED) ?
+                     "selected":"unselected");
+        output.append(s + newline);
+        output.setCaretPosition(output.getDocument().getLength());
+    }
+
+    // Returns just the class name -- no package info.
+    protected String getClassName(Object o) {
+        String classString = o.getClass().getName();
+        int dotIndex = classString.lastIndexOf(".");
+        return classString.substring(dotIndex+1);
+    }
+    
+    protected static void createAndShowGUI() {
+        //Create and set up the window.
+        /*JFrame frame = new JFrame("MenuDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        menuBar demo = new menuBar();
+        frame.setJMenuBar(demo.createMenuBar());
+        frame.setContentPane(demo.createContentPane());
+
+        //Display the window.
+        frame.setSize(450, 260);
+        frame.setVisible(true);
+        */
+    }
+    
+    protected class SortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
+    
+    private ListNode<T> top;
+    private ListNode<T> temp;
+    private ListNode<T> head;
+    private ListNode<T> tail;
+    
+    public SortedLinkedPriorityQueue() {
+        top = null;
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+    
+    @Override
+    public T head() throws QueueUnderflowException{
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+        return head.getItem();
+        }
+    }
+    
+    @Override
+    public void remove() throws QueueUnderflowException{
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        }
+        head = head.getNext();
+    }
+    
+    public void add(T item, int priority) throws QueueOverflowException {
+        { 
+         ListNode current; 
+         ListNode new_node = new ListNode(item, head, priority);
+  
+         /* Special case for head node */
+         if (head == null || head.priority <= new_node.priority) 
+         { 
+            new_node.next = head; 
+            head = new_node; 
+            top = head;
+         } 
+         else { 
+  
+            /* Locate the node before point of insertion. */
+            current = head; 
+  
+            while (current.next != null && 
+                   current.next.priority > new_node.priority) 
+                  current = current.next; 
+  
+            new_node.next = current.next; 
+            current.next = new_node; 
+         } 
+     } 
+    }
+    @Override
+    public String toString() {
+        String result = "";
+         ListNode temp = head; 
+         while (temp != null) 
+         { 
+            //System.out.print(temp.priority+" "); 
+             result += temp.getItem();
+             if(temp.next != null){
+                     result += ", ";
+                } 
+            temp = temp.next; 
+         } /*
+         if (temp.next == null){
+                result += temp.getItem();
+            }*/
+            return "List: " + result;
+     }
+    
+    private int size() {
+        ListNode<T> node = top;
+        int result = 0;
+        while (node != null) {
+            result = result + 1;
+            node = node.getNext();
+        }
+        return result;
+    }
+}
 }
