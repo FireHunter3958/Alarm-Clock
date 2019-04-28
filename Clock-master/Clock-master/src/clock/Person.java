7503272d8a769454dedaa286a94a4633c5a50703
FireHunter3958 <47977209+FireHunter3958@users.noreply.github.com>
2019-04-28 20:09:13 +0100

    Add files via upload
    Able to display stored values (requires work, order of storage is occasionally incorrect).

---
+++ Clock-master/Clock-master/src/clock/Person.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clock;
+
+/**
+ *
+ * @author FireHunter3958
+ */
+public class Person {
+
+    protected String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
