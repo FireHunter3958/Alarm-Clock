a8fe9d92c9fa5cb081c638d3a020008716428ca6
FireHunter3958 <47977209+FireHunter3958@users.noreply.github.com>
2019-05-12 16:11:30 +0100

    Add files via upload
    Some bits of code tidied up (small update, not entirely necessary).

--- Clock-master/Clock-master/src/clock/View.java
+++ Clock-master/Clock-master/src/clock/View.java
@@ -5,8 +5,19 @@ import javax.swing.*;
 import java.util.Observer;
 import java.util.Observable;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JCheckBoxMenuItem;
@@ -16,15 +27,39 @@ import javax.swing.JMenuBar;
 import javax.swing.KeyStroke;
 import javax.swing.ImageIcon;
 
-public class View implements Observer {
-    
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//https://www.javaxp.com/2012/05/java-simple-icalendar-ical4j-example.html
+
+
+
+
+public class View implements Observer, ActionListener, ItemListener {
     ClockPanel panel;
-    ClockPanel pane2;
+    //ClockPanel pane2;
+    JTextArea output;
+    JScrollPane scrollPane;
+    String newline = "\n";
     
+    //https://stackoverflow.com/questions/683041/how-do-i-use-a-priorityqueue
+    //Comparable<Integer> comparator = new IntegerComparator();
+    //PriorityQueue<Integer> queue = new PriorityQueue<Integer>(8, comparator);
+    //PriorityQueue<Integer> alarmQueue = new PriorityQueue<Integer>();
+    Alarm alarm =  new Alarm();
+    String editValueString;
+    
+    Model Model = new Model();
+    PriorityQueue<Integer> alarmQueue = Model.alarmQueue;
+    PriorityQueue<String> alarmView = Model.alarmView;
     public View(Model model) {
         JFrame frame = new JFrame();
         panel = new ClockPanel(model);
-        pane2 = new ClockPanel(model);
+        //pane2 = new ClockPanel(model);
         JMenuBar menuBar;
         JMenu menu, submenu;
         JMenuItem menuItem;
@@ -40,75 +75,45 @@ public class View implements Observer {
         menuBar = new JMenuBar();
 
         //Build the first menu.
-        menu = new JMenu("A Menu");
+        menu = new JMenu("Menu");
         menu.setMnemonic(KeyEvent.VK_A);
         menu.getAccessibleContext().setAccessibleDescription(
                 "The only menu in this program that has menu items");
         menuBar.add(menu);
-
-        //a group of JMenuItems
-        menuItem = new JMenuItem("A text-only menu item",
-                                 KeyEvent.VK_T);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+        
+        JMenuItem menuItemV2;
+        menuItemV2 = new JMenuItem(new FileAlarm());
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuItemV2.setAccelerator(KeyStroke.getKeyStroke(
                 KeyEvent.VK_1, ActionEvent.ALT_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription(
+        menuItemV2.getAccessibleContext().setAccessibleDescription(
                 "This doesn't really do anything");
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem("Both text and icon",
-                                 new ImageIcon("images/middle.gif"));
-        menuItem.setMnemonic(KeyEvent.VK_B);
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
-        menuItem.setMnemonic(KeyEvent.VK_D);
-        menu.add(menuItem);
-
-        //a group of radio button menu items
-        menu.addSeparator();
-        ButtonGroup group = new ButtonGroup();
-        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_R);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        rbMenuItem = new JRadioButtonMenuItem("Another one");
-        rbMenuItem.setMnemonic(KeyEvent.VK_O);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        //a group of check box menu items
-        menu.addSeparator();
-        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
-        cbMenuItem.setMnemonic(KeyEvent.VK_C);
-        menu.add(cbMenuItem);
-
-        cbMenuItem = new JCheckBoxMenuItem("Another one");
-        cbMenuItem.setMnemonic(KeyEvent.VK_H);
-        menu.add(cbMenuItem);
-
-        //a submenu
-        menu.addSeparator();
-        submenu = new JMenu("A submenu");
-        submenu.setMnemonic(KeyEvent.VK_S);
-
-        menuItem = new JMenuItem("An item in the submenu");
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_2, ActionEvent.ALT_MASK));
-        submenu.add(menuItem);
-
-        menuItem = new JMenuItem("Another item");
-        submenu.add(menuItem);
-        menu.add(submenu);
-
-        //Build second menu in the menu bar.
-        menu = new JMenu("Another Menu");
-        menu.setMnemonic(KeyEvent.VK_N);
-        menu.getAccessibleContext().setAccessibleDescription(
-                "This menu does nothing");
-        menuBar.add(menu);
-
+        //q = new SortedPriorityQueue();
+        menuItemV2.addActionListener(this);
+        menu.add(menuItemV2);
+        
+        JMenuItem menuItemV3;
+        menuItemV3 = new JMenuItem(new ViewAlarm());
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuItemV3.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItemV3.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        //q = new SortedPriorityQueue();
+        menuItemV3.addActionListener(this);
+        menu.add(menuItemV3);
+        
+        JMenuItem menuSave;
+        menuSave = new JMenuItem(new ButtonLoad());
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuSave.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuSave.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        //q = new SortedPriorityQueue();
+        menuSave.addActionListener(this);
+        menu.add(menuSave);
+        
         
         frame.setJMenuBar(menuBar);
         // Start of border layout code
@@ -130,14 +135,32 @@ public class View implements Observer {
         panel.setPreferredSize(new Dimension(200, 200));
         pane.add(panel, BorderLayout.CENTER);
          
-        button = new JButton("Button 3 (LINE_START)");
+        button = new JButton(new AddAlarm());
         pane.add(button, BorderLayout.LINE_START);
-         
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
+        //q = new SortedLinkedPriorityQueue<>();
+        
+        output = new JTextArea(5, 30);
+        output.setEditable(false);
+        JScrollPane paneScroll = new JScrollPane(output);
+        
+        button = new JButton(new ViewAlarm());
+        pane.add(button, BorderLayout.PAGE_START);
+        //q = new SortedLinkedPriorityQueue<>();
+
+        //Add the text area to the content pane.
+
+        //Add the text area to the content pane.
+        pane.add(paneScroll, BorderLayout.PAGE_END);
+         /*
+        button = new JButton("End Program (End Program)");
         pane.add(button, BorderLayout.PAGE_END);
+        
+         */
          
-        button = new JButton("5 Add Alarm(LINE_END)");
+        button = new JButton(new EditAlarm());
         pane.add(button, BorderLayout.LINE_END);
+        //button = new JButton("5 Add Alarm(Edit Alarm)");
+        //pane.add(button, BorderLayout.LINE_END);
         /*
         button = new JButton("5 Add Alarm(LINE_END)");
         pane.add(button, BorderLayout.BEFORE_FIRST_LINE);
@@ -153,4 +176,123 @@ public class View implements Observer {
     public void update(Observable o, Object arg) {
         panel.repaint();
     }
+    
+    public Container createContentPane() {
+        //Create the content-pane-to-be.
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setOpaque(true);
+
+        //Create a scrolled text area.
+        output = new JTextArea(5, 30);
+        output.setEditable(false);
+        scrollPane = new JScrollPane(output);
+
+        //Add the text area to the content pane.
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+
+        return contentPane;
+    }
+    
+    protected class AddAlarm extends AbstractAction {
+        public AddAlarm() {
+        super ("Add Alarm");
+                }
+        public void actionPerformed(ActionEvent v) {
+            Model.actionPerformed(v);
+            output.append("Queue value is: " + alarmView);
+            output.append("\n");
+            
+          
+        }
+    }
+    
+    protected class ViewAlarm extends AbstractAction {
+        public ViewAlarm() {
+        super ("View Alarms");
+                }
+        public void actionPerformed(ActionEvent v) {
+            String queueView = String.valueOf(alarmQueue);
+            output.append(queueView);
+        }
+    }
+    
+    protected class FileAlarm extends AbstractAction {
+        public FileAlarm() {
+        super ("Add Alarm to File");
+                }
+        //@Override
+        @Override
+        public void actionPerformed(ActionEvent r)  {
+            Model.write("AlarmList");
+            Model.StringWrite("AlarmListString");
+
+        }
+    }
+    
+    protected class ButtonLoad extends AbstractAction {
+        public ButtonLoad() {
+            super ("Load Alarms from File");
+        }
+        public void actionPerformed(ActionEvent s) {
+            try {
+                Model.Load("AlarmList.ics");
+                Model.StringLoad("AlarmListString.ics");
+            } catch (IOException ex) {
+                Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+            } 
+    }
+    
+    //https://stackoverflow.com/questions/31238492/writing-ics-ical-file-using-java
+    
+    
+    protected class EditAlarm extends AbstractAction {
+        public EditAlarm() {
+        super ("Edit Alarms");
+                }
+        public void actionPerformed(ActionEvent n) {
+            Model.EditAlarm(n);
+            output.append("\n");
+            output.append("Queue value is: " + alarmView);
+            
+        }
+    }
+    
+    
+    
+    public void actionPerformed(ActionEvent e) {
+        JMenuItem source = (JMenuItem)(e.getSource());
+        String s = "Action event detected."
+                   + newline
+                   + "    Event source: " + source.getText()
+                   + " (an instance of " + getClassName(source) + ")";
+        output.append(s + newline);
+        output.setCaretPosition(output.getDocument().getLength());
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        JMenuItem source = (JMenuItem)(e.getSource());
+        String s = "Item event detected."
+                   + newline
+                   + "    Event source: " + source.getText()
+                   + " (an instance of " + getClassName(source) + ")"
+                   + newline
+                   + "    New state: "
+                   + ((e.getStateChange() == ItemEvent.SELECTED) ?
+                     "selected":"unselected");
+        output.append(s + newline);
+        output.setCaretPosition(output.getDocument().getLength());
+    }
+
+    // Returns just the class name -- no package info.
+    protected String getClassName(Object o) {
+        String classString = o.getClass().getName();
+        int dotIndex = classString.lastIndexOf(".");
+        return classString.substring(dotIndex+1);
+    }
+    
+    protected static void createAndShowGUI() {
+        
+    }
+    
 }
